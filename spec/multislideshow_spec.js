16f79cbb223ec52510e7db8c36a0fc2561a1f1df
Zach H <zdhadgraft@gtcc.edu>
2017-01-06 00:34:02 -0500

    Refactored variables.

--- spec/multislideshow_spec.js
+++ spec/multislideshow_spec.js
@@ -33,17 +33,19 @@ describe("MultiSlideshow", function(){
 
     describe("post-initialization", function(){
 
+        var ms, e;
+
         beforeEach(function(){
-            this.ms = new MultiSlideshow(testSlides);
-            this.e = new MockElement();
+            ms = new MultiSlideshow(testSlides);
+            e = new MockElement();
         });
 
         describe("#bindElement", function(){
 
             it("will throw an error if first param is not an array", function(){
                 expect(function(){
-                    this.ms.bindElement(
-                        this.e,
+                    ms.bindElement(
+                        e,
                         'mockDesc',
                         'desc');})
                     .toThrowError(TypeError);
@@ -51,8 +53,8 @@ describe("MultiSlideshow", function(){
 
             it("will throw an error if second param is not a string", function(){
                 expect(function(){
-                    this.ms.bindElement(
-                        this.e,
+                    ms.bindElement(
+                        e,
                         undefined,
                         'desc');})
                     .toThrowError(TypeError);
@@ -60,32 +62,32 @@ describe("MultiSlideshow", function(){
 
             it("will throw an error if third param is not given", function(){
                 expect(function(){
-                    this.ms.bindElement(
-                        this.e,
+                    ms.bindElement(
+                        e,
                         'mockDesc');})
                     .toThrowError(TypeError);
             });
 
             it("is used to make a property of a slide assignable to a property of an element", function(){
-                this.ms.bindElement(
-                    [this.e],
+                ms.bindElement(
+                    [e],
                     'mockDesc',
                     'desc'
                 );
-                this.ms.setSlide(1);
-                expect(this.e.mockDesc).toEqual('desc2');
+                ms.setSlide(1);
+                expect(e.mockDesc).toEqual('desc2');
             });
 
             it("is used to make a property of a slide assignable to a property of multiple elements", function(){
                 this.f = new MockElement();
-                this.ms.bindElement(
-                    [this.e, this.f],
+                ms.bindElement(
+                    [e, this.f],
                     'mockDesc',
                     'desc'
                 );
 
-                this.ms.setSlide(1);
-                expect(this.e.mockDesc).toEqual('desc2');
+                ms.setSlide(1);
+                expect(e.mockDesc).toEqual('desc2');
                 expect(this.f.mockDesc).toEqual('desc2');
             });
 
@@ -94,33 +96,33 @@ describe("MultiSlideshow", function(){
         describe("#setSlide", function(){
 
             it("changes the current slide to index corresponding to the original object", function(){
-                this.ms.setSlide(0);
-                this.ms.setSlide(2);
-                expect(this.ms.currentSlide).toEqual(testSlides[2]);
+                ms.setSlide(0);
+                ms.setSlide(2);
+                expect(ms.currentSlide).toEqual(testSlides[2]);
             });
 
         });
 
         describe("#bindEvent", function(){
             it("an event changes the current slide to a specific slide", function(){
-                this.ms.setSlide(0);
-                this.ms.bindEvent(2, this.e, 'click');
-                this.e.click();
-                expect(this.ms.currentSlide).toEqual(testSlides[2]);
+                ms.setSlide(0);
+                ms.bindEvent(2, e, 'click');
+                e.click();
+                expect(ms.currentSlide).toEqual(testSlides[2]);
             });
 
             it("when applied through #bindNextEvent, it changes to the next 'slide'", function(){
-                this.ms.setSlide(0);
-                this.ms.bindNextEvent(this.e, 'click');
-                this.e.click();
-                expect(this.ms.currentSlide).toEqual(testSlides[1]);
+                ms.setSlide(0);
+                ms.bindNextEvent(e, 'click');
+                e.click();
+                expect(ms.currentSlide).toEqual(testSlides[1]);
             });
 
             it("when applied through #bindPrevEvent, it changes to the previous 'slide'", function(){
-                this.ms.setSlide(0);
-                this.ms.bindPrevEvent(this.e, 'click');
-                this.e.click();
-                expect(this.ms.currentSlide).toEqual(testSlides.splice(-1,1)[0]);
+                ms.setSlide(0);
+                ms.bindPrevEvent(e, 'click');
+                e.click();
+                expect(ms.currentSlide).toEqual(testSlides.splice(-1,1)[0]);
             });
 
         });
